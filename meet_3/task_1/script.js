c3849dcbef4dae6675e78067a0422f35d706b27b
Inna <inna.avanesian@gmail.com>
2025-07-29 17:24:15 +0300

    added files

---
+++ meet_3/task_1/script.js
@@ -0,0 +1,12 @@
+"use strict"
+
+//Вивести на сторінку в один рядок через кому числа від 10 до 20.
+
+for (let i = 10; i < 21; i++) {
+    console.log(i);
+    let num = document.querySelector("#numbres").innerHTML = i;
+}
+
+for (let i = 0; i < 3; i++) {
+    console.log(i);
+}
